0d808dbadca67eb355cebc8cef24c140b8ad23fb
imloama <imloama@gmail.com>
2019-04-09 15:49:19 +0800

    init

---
+++ packages/core/dist/types/typescript.d.ts
@@ -0,0 +1,8 @@
+export declare type Diff<T extends string, U extends string> = ({
+    [P in T]: P;
+} & {
+    [P in U]: never;
+} & {
+    [x: string]: never;
+})[T];
+export declare type Omit<T, K> = Pick<T, Exclude<keyof T, K>>;
---
+++ packages/core/dist/utils/constants.d.ts
@@ -0,0 +1,4 @@
+import { ThemePair } from '../types';
+export declare const DEFAULT_DARK_THEME = "dark-gray";
+export declare const DEFAULT_LIGHT_THEME = "light-blue";
+export declare const DEFAULT_THEME_PAIR: ThemePair;
